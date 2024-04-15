a704cc3c20cd71ca65db6d39f6cd10f60b5260dd
Wim Roukema <wim.roukema@gmail.com>
2024-04-15 16:07:17 +0200

    Added dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -14,6 +14,10 @@ public class Application {
 		}
 	}
 
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, " ");
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 	public Application() {
 		System.out.println("Inside Application");
 	}
@@ -23,5 +27,7 @@ public class Application {
 		System.out.println("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Word count "+count);
 	}
 }
